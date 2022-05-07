ae1a3819ac32b3b245ef5e27d5e026b30276eafa
jinchunglee <jameslee9168@gmail.com>
2022-05-07 19:58:21 +0800

    測試一

---
+++ c language/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c language
+//
+//  Created by James Lee on 2022/5/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("hhh");
+    return 0;
+}
